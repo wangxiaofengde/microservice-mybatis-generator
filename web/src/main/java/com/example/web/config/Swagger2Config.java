7805c375d1341762ad613c34ccd43e15c9abfc4d
wangxiaofengde <wang181feng@163.com>
2019-09-17 00:33:25 +0800

    swagger setting global param

--- web/src/main/java/com/example/web/config/Swagger2Config.java
+++ web/src/main/java/com/example/web/config/Swagger2Config.java
@@ -1,13 +1,19 @@
 package com.example.web.config;
 
+import io.swagger.annotations.Api;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration //标记配置类
 @EnableSwagger2 //开启在线接口文档
@@ -15,7 +21,7 @@ public class Swagger2Config {
     /**
      * 添加摘要信息(Docket)
      */
-    @Bean
+    //@Bean
     public Docket controllerApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .apiInfo(new ApiInfoBuilder()
@@ -28,4 +34,42 @@ public class Swagger2Config {
                 .paths(PathSelectors.any())
                 .build();
     }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2).
+                useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("^(?!auth).*$"))
+                .build()
+                .securitySchemes(securitySchemes())
+                .securityContexts(securityContexts())
+                ;
+    }
+    private List<ApiKey> securitySchemes() {
+        List list =  new ArrayList();
+        list.add(new ApiKey("token", "token", "header"));
+        return list;
+    }
+    private List<SecurityContext> securityContexts() {
+        List list =  new ArrayList();
+         list.add(
+                SecurityContext.builder()
+                        .securityReferences(defaultAuth())
+                        .forPaths(PathSelectors.regex("^(?!auth).*$"))
+                        .build());
+         return list;
+
+    }
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        List list =  new ArrayList();
+         list.add(
+                new SecurityReference("token", authorizationScopes));
+         return list;
+    }
+
 }
--- web/src/main/java/com/example/web/controller/TestContoller.java
+++ web/src/main/java/com/example/web/controller/TestContoller.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
 
 @Api("测试")
 @RestController
 @RequestMapping("test")
 public class TestContoller {
+    @Resource
+    private HttpServletRequest request;
     @ApiOperation("添加")
     @PostMapping("/add")
     public void add(int a ,String name){
